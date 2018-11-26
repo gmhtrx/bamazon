784ce176699e56e09007815b6b122801fa6b0480
Chan Lee <chano@Chans-MacBook-Pro.local>
2018-11-26 15:15:59 -0500

    chan

---
+++ inventoryseed.sql
@@ -0,0 +1,11 @@
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("Blonde", "Music", 12.99, 100),
+	("Good AM", "Music", 12.99, 100),
+    ("Yeezus", "Music", 12.99, 100),
+    ("Ray Bans", "Apparel", 120.99, 100),
+    ("Chino Pants", "Apparel", 29.99, 100),
+    ("T-Shirts", "Apparel", 20.99, 100),
+    ("Starcraft 2", "Video Games", 49.99, 100),
+    ("Diablo 3", "Video Games", 49.99, 100);
+    
+SELECT * FROM bamazon.products;
